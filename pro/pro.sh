5bfe3b4a726ee3e0214011f09dd28e4a01345d22
coolbele <64392071+coolbele@users.noreply.github.com>
2025-02-09 11:36:27 +0800

    Update pro.sh

--- pro/pro.sh
+++ pro/pro.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install make -y
-git clone -b main --single-branch https://github.com/sdf8057/ipq6000.git openwrt
+git clone -b main --single-branch https://github.com/coolbele/v6.git openwrt
 cd openwrt
 ./scripts/feeds update -a && ./scripts/feeds install -a
 wget https://raw.githubusercontent.com/ppayjjk/ipq6000/main/pro/.config
